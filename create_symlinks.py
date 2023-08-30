447e656d1be12a970f49ace6a542906e25240e4d
Bruno Campos <brunofdcampos@hotmail.com>
2023-08-30 23:01:30 +0100

    auto install neovim
    Signed-off-by: Bruno Campos <brunofdcampos@hotmail.com>

--- create_symlinks.py
+++ create_symlinks.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import subprocess
+import platform
 
 def create_symlink(source, destination):
     if os.path.exists(destination):
@@ -10,15 +12,35 @@ def create_symlink(source, destination):
 
     os.symlink(source, destination)
 
+def install_neovim():
+    os_type = platform.system()
+    if os_type == "Linux":
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "install", "neovim"])
+    elif os_type == "Darwin":
+        subprocess.run(["brew", "install", "neovim"])
+    else:
+        print(f"Unsupported OS: {os_type}")
+        return
+
+    nvim_config_dir = os.path.expanduser('~/.config/nvim')
+    if not os.path.exists(nvim_config_dir):
+        os.makedirs(nvim_config_dir)
+
+    init_vim_source = os.path.expanduser('~/dotfiles/init.vim')
+    init_vim_dest = os.path.expanduser('~/.config/nvim/init.vim')
+    create_symlink(init_vim_source, init_vim_dest)
+
 config_mapping = {
     'bashrc': '~/.bashrc',
-    #'bash_profile': '~/.bash_profile',
     'zshrc': '~/.zshrc',
+    'p10k.zsh': '~/.p10k.zsh',
+    'zsh_history': '~/.zsh_history',
 }
 
 if __name__ == "__main__":
     for source, dest in config_mapping.items():
-        src_path = os.path.expanduser(f'~/GitRepos/dotfiles/{source}')
+        src_path = os.path.expanduser(f'~/dotfiles/{source}')
         dest_path = os.path.expanduser(dest)
         create_symlink(src_path, dest_path)
 
--- init.vim
+++ init.vim
@@ -17,6 +17,17 @@ set mouse=
 " Open split windows below
 set splitbelow
 
+" Install vim-plug if not found
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+endif
+
+" Run PlugInstall if there are missing plugins
+autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
+  \| PlugInstall --sync | source $MYVIMRC
+\| endif
+
 " Begin Plug configuration
 call plug#begin()
 
